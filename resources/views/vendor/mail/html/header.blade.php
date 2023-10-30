a0dd5255f872502e66a7aa2c2fdf6f877af3d574
Carolina Praxedes <ana_carolina47@hotmail.com>
2023-10-29 22:24:02 -0300

    inicio do projeto

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,12 @@
+@props(['url'])
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            @if (trim($slot) === config('app.name'))
+                <img src="{{ asset('img/logo.png') }}" class="logo" alt="Miu Tasks Logo">
+            @else
+                {{ $slot }}
+            @endif
+        </a>
+    </td>
+</tr>
